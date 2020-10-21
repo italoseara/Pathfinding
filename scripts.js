b8373d828ccb5985d1003338df30ce050cdc9ae2
Italo Seara <54297273+italoseara@users.noreply.github.com>
2020-10-21 16:52:27 -0300

    Animation

--- scripts.js
+++ scripts.js
@@ -6,18 +6,28 @@ setInterval(update, 100);
 
 function drawBoxes(){
     var boxes = '';
-    var starter = [10, 7];
-    var final = [10, 28]
+    var starter = [16, 10];
+    var final = [16, 48]
 
-    for (var i = 0; i < 720; i++) {
-        if (i === 36*(starter[0] - 1) + starter[1]) {
-            boxes += '<div class="box starter"></div>';
-        } else if (i === 36*(final[0] - 1) + final[1]) {
-            boxes += '<div class="box final"></div>';
-        } else boxes += '<div class="box"></div>';
+    for (var row = 0; row < 32; row++) {
+        document.querySelector('.grid').innerHTML += '<div class="row row' + row + '"></div>';
+        for (var col = 0; col < 58; col++) {
+            document.querySelector('.row' + row).innerHTML += '<div class="box"></div>';
+        }
     }
 
-    document.querySelector('.grid').innerHTML = boxes;
+    document.querySelector('.row' + (starter[0] - 1)).querySelectorAll('.box')[starter[1]].classList.add('starter')
+    document.querySelector('.row' + (final[0] - 1)).querySelectorAll('.box')[final[1]].classList.add('final')
+}
+
+function popOut(object) {
+    object.style.transitionDuration = '0.15s';
+    object.style.transform = 'scale(1.3)';
+
+    setTimeout(() => {
+        object.style.transitionDuration = '0.07s';
+        object.style.transform = 'scale(1)';
+    }, 150);
 }
 
 function update() {
@@ -25,10 +35,11 @@ function update() {
     document.querySelector('.final').classList.remove('wall');
 }
 
-document.querySelector('.grid').addEventListener('mousedown', () => { walling = true; currentBox.classList.add('wall');})
+document.querySelector('.grid').addEventListener('mousedown', () => { walling = true; currentBox.classList.add('wall'); popOut(currentBox);})
 document.querySelector('.grid').addEventListener('mouseup', () => walling = false)
 document.querySelector('.grid').addEventListener('mousemove', () => {
     currentBox = document.querySelector('.box:hover');
     if (!walling) return;
     currentBox.classList.add('wall');
+    popOut(currentBox);
 })
